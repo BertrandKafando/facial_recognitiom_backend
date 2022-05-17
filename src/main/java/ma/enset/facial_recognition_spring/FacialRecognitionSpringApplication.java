2db1da937a1bef4f6466bba9ea90e7d1d925b94c
bertrand <bertrandkafando07@gmail.com>
2022-05-17 22:33:08 +0100

    first commit

---
+++ src/main/java/ma/enset/facial_recognition_spring/FacialRecognitionSpringApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.facial_recognition_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialRecognitionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacialRecognitionSpringApplication.class, args);
+    }
+
+}
