5339677431c16c0496f662c26a6cd8da283fa384
bertrand <bertrandkafando07@gmail.com>
2022-05-27 00:43:13 +0100

    affichage

--- src/main/java/ma/enset/facial_recognition_spring/service/FacialService.java
+++ src/main/java/ma/enset/facial_recognition_spring/service/FacialService.java
@@ -1,5 +1,6 @@
 package ma.enset.facial_recognition_spring.service;
 
+import ma.enset.facial_recognition_spring.entities.DateStudent;
 import ma.enset.facial_recognition_spring.entities.Present;
 import ma.enset.facial_recognition_spring.entities.PresentAndAbsent;
 import ma.enset.facial_recognition_spring.entities.Student;
@@ -17,7 +18,7 @@ public interface FacialService {
     List<Present>getAllPresentStudent(Date t1, Date t2);
     List<Student>getAllAbsentStudent(Date t1, Date t2);
 
-    List<Student> getAllPresents(Date t1, Date t2);
+    List<DateStudent> getAllPresents(Date t1, Date t2);
 
     List<PresentAndAbsent> getPresentsAndAbsent(Date t1, Date t2);
 }
