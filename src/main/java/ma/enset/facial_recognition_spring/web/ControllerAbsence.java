b01659a374612ab454543aabcb90bbdf52ff3d38
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-06-10 12:06:34 +0100

    final ...

--- src/main/java/ma/enset/facial_recognition_spring/web/ControllerAbsence.java
+++ src/main/java/ma/enset/facial_recognition_spring/web/ControllerAbsence.java
@@ -28,19 +28,33 @@ public class ControllerAbsence {
                           @RequestParam(name = "t1", defaultValue = "21:30:20") String t1,
                           @RequestParam(name = "t2", defaultValue = "22:46:20") String t2,
                           @RequestParam(name = "aff", defaultValue = "") String aff) throws ParseException {
-        List<PresentAndAbsent> presentAndAbsentList = facialService.getPresentsAndAbsent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
-        List<Student> absents = facialService.getAllAbsentStudent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
-        List<DateStudent> presents = facialService.getAllPresents(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
-        String aff1 = aff;
-        String date = "Absence du "+d;
-        String time = "De "+t1+" à "+t2;
 
-        model.addAttribute("presentAndAbsents",presentAndAbsentList);
-        model.addAttribute("absents",absents);
-        model.addAttribute("presents",presents);
-        model.addAttribute("aff",aff1);
-        model.addAttribute("time",time);
-        model.addAttribute("date1",date);
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00");
+        Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00");
+        String erreur ="";
+        if(date1.after(date2)) {
+            erreur = "Merci de verifier les dates choisies";
+        }
+            List<PresentAndAbsent> presentAndAbsentList = facialService.getPresentsAndAbsent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
+            List<Student> absents = facialService.getAllAbsentStudent(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
+            List<DateStudent> presents = facialService.getAllPresents(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t1+":00"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(d+" "+t2+":00"));
+            String aff1 = aff;
+            String date = "Absence du "+d;
+            String time = "De "+t1+" à "+t2;
+
+            model.addAttribute("presentAndAbsents",presentAndAbsentList);
+            model.addAttribute("absents",absents);
+            model.addAttribute("presents",presents);
+            model.addAttribute("aff",aff1);
+            model.addAttribute("time",time);
+            model.addAttribute("date1",date);
+
+
+            model.addAttribute("erreur",erreur);
+
+
+
+
         return "principale";
     }
    @GetMapping("/")
